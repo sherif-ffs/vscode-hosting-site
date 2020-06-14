fda8c2421cad9b55b71a2df5333a64ff46fdc7e5
Sherif Elmetwally <sge5@njit.edu>
2020-06-14 01:04:04 -0400

    add vscode logo and color palette

---
+++ asda.js
@@ -0,0 +1,35 @@
+document.querySelector('#editor-background').addEventListener('mouseover', () => {
+    document.querySelector('#editor-background-flag').style.visibility = 'visible'
+})
+document.querySelector('#editor-background').addEventListener('mouseleave', () => {
+    document.querySelector('#editor-background-flag').style.visibility = 'hidden'
+})
+
+document.querySelector('#main-pink').addEventListener('mouseover', () => {
+    document.querySelector('#main-pink-flag').style.visibility = 'visible'
+})
+
+document.querySelector('#main-pink').addEventListener('mouseleave', () => {
+    document.querySelector('#main-pink-flag').style.visibility = 'hidden'
+})
+
+document.querySelector('#editor-foreground').addEventListener('mouseover', () => {
+    document.querySelector('#editor-foreground-flag').style.visibility = 'visible'
+})
+document.querySelector('#editor-foreground').addEventListener('mouseleave', () => {
+    document.querySelector('#editor-foreground-flag').style.visibility = 'hidden'
+})
+
+document.querySelector('#yellow').addEventListener('mouseover', () => {
+    document.querySelector('#yellow-flag').style.visibility = 'visible'
+})
+document.querySelector('#yellow').addEventListener('mouseleave', () => {
+    document.querySelector('#yellow-flag').style.visibility = 'hidden'
+})
+
+document.querySelector('#main-blue').addEventListener('mouseover', () => {
+    document.querySelector('#main-blue-flag').style.visibility = 'visible'
+})
+document.querySelector('#main-blue').addEventListener('mouseleave', () => {
+    document.querySelector('#main-blue-flag').style.visibility = 'hidden'
+})
